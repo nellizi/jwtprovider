660d33acb7b35cc0d86bf9ee7e2fa0cb808a83ab
nellizi <masterliz9095@gmail.com>
2022-10-05 02:01:13 +0900

    accessToken 생성

--- src/main/java/com/ll/exam/app__2022_10_04/app/jwt/JwtProvider.java
+++ src/main/java/com/ll/exam/app__2022_10_04/app/jwt/JwtProvider.java
@@ -1,9 +1,14 @@
 package com.ll.exam.app__2022_10_04.app.jwt;
 
+import com.ll.exam.app__2022_10_04.util.Util;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
 
 @Component
 @AllArgsConstructor
@@ -12,5 +17,15 @@ public class JwtProvider {
 
     private SecretKey getSecretKey() {
         return jwtSecretKey;
+    }
+    public String generateAccessToken(Map<String, Object> claims, int seconds) {
+        long now = new Date().getTime();
+        Date accessTokenExpiresIn = new Date(now + 1000L * seconds);
+
+        return Jwts.builder()
+                .claim("body", Util.json.toStr(claims))
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
     }
 }
